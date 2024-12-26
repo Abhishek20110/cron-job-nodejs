543de7468ddc3865821d6f9d0a9bc51ee6bfa199
Abhishek Dey <71166991+Abhishek20110@users.noreply.github.com>
2024-12-26 18:31:12 +0530

    res

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ const cron = require('node-cron');
 const app = express();
 const PORT = 3000;
 
+// Variable to store the latest cron job response
+let cronResponse = '';
+
 // Route to handle requests to the root URL
 app.get('/', (req, res) => {
     res.send('Welcome to the back end');
 });
 
+// Route to get the latest cron response
+app.get('/cron-response', (req, res) => {
+    res.send(cronResponse || 'No cron response yet');
+});
+
 // Schedule the task to run every 2 minutes
 cron.schedule('*/2 * * * *', () => {
+    cronResponse = 'This is a cron response';
     console.log('Hello World');
+    console.log(cronResponse);
 });
 
 console.log('Cron job service started...');
