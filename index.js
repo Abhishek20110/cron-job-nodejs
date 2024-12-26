35482ffc6bb97c3460ed2cbf136aa003d3df8f53
Abhishek Dey <71166991+Abhishek20110@users.noreply.github.com>
2024-12-26 18:12:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+const express = require('express');
 const cron = require('node-cron');
+
+const app = express();
+const PORT = 3000;
+
+// Route to handle requests to the root URL
+app.get('/', (req, res) => {
+    res.send('Welcome to the back end');
+});
 
 // Schedule the task to run every 2 minutes
 cron.schedule('*/2 * * * *', () => {
@@ -6,3 +15,8 @@ cron.schedule('*/2 * * * *', () => {
 });
 
 console.log('Cron job service started...');
+
+// Start the Express server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
